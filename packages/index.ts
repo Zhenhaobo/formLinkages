cc1be441717aa10f977f4808c54acc97a0cb8134
zhenhaobo <zhenhaobo@bytedance.com>
2023-12-08 19:09:33 +0800

    feat: init

---
+++ packages/index.ts
@@ -0,0 +1,23 @@
+export * from './utils'
+
+import zhbButton from './themeButton/index'
+// 以数组的结构保存组件，便于遍历
+const components = [zhbButton]
+// 用于按需导入
+export { zhbButton }
+// 定义 install 方法
+const install = function (Vue: any) {
+  if ((install as any).installed) return
+  ;(install as any).installed = true // 遍历并注册全局组件
+  components.map((component) => {
+    Vue.component(component.name, component)
+  })
+} 
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+export default {
+  // 导出的对象必须具备一个 install 方法
+  install,
+  ...components
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import vueJsx from '@vitejs/plugin-vue-jsx' // pnpm add -D @vitejs/plugin-vue-jsx
+import path from 'path'
+import dts from 'vite-plugin-dts'
+const resolve = (dir) => path.join(__dirname, dir)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx({}),dts()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve('examples'),
+      packages: resolve('packages')
+    }
+  },
+  build: {
+    rollupOptions: {
+      // 请确保外部化那些你的库中不需要的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+    lib: {
+      entry: 'packages/index.ts',
+      name: 'zhb_test',
+      fileName: (format) => `zhb_test.${format}.js`
     }
   }
 })
